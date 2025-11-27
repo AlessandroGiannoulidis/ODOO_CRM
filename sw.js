2ad2fd4699a1dff997780bdf8ad5b565116fa0e7
AlessandroGiannoulidis <alessandro.giannoulidis.siak@gmail.com>
2025-11-27 17:05:08 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,31 +1,3 @@
-const CACHE_NAME = 'pwa-redirect-cache-v1';
-const ASSETS = [
-'/',
-'/index.html',
-'/manifest.json',
-'/icon-100.png'
-];
-
-
-self.addEventListener('install', event => {
-event.waitUntil(
-caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-);
-});
-
-
-self.addEventListener('activate', event => {
-event.waitUntil(
-caches.keys().then(keys => Promise.all(
-keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-))
-);
-});
-
-
-self.addEventListener('fetch', event => {
-// Serve risorse dalla cache, fallback a network
-event.respondWith(
-caches.match(event.request).then(cached => cached || fetch(event.request))
-);
+self.addEventListener('fetch', function(event) {
+  event.respondWith(fetch(event.request));
 });
